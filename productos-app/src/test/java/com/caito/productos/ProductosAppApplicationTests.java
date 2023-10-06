e4cda418a7879268f4d733f6eeb7b786f6673d07
claudio.vilas <caitocd@gmail.com>
2023-10-06 13:05:59 -0300

    first commit

---
+++ productos-app/src/test/java/com/caito/productos/ProductosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caito.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
